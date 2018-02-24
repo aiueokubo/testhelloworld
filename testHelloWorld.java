2b6d390d8043b56e2ad7b826d01bdfb291381668
OKUBO\aiueokubo <this.ma11.is.sent.by.mr.k@gmail.com>
2018-02-24 15:09:48 +0900

    2018/02/24 Test2

--- testHelloWorld.java
+++ testHelloWorld.java
@@ -4,8 +4,8 @@ public class testHelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World!");
-		System.out.print("TestOKB");
-
+		System.out.println("TestOKB");
+		System.out.print("testOKB2");
 	}
 
 }
